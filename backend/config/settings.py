d793ee6421e3657cb3fb94fc5eba2faf740a4d15
mussietarekegn <mussie.tarekegn@aastustudent.edu.et>
2026-04-16 22:21:28 +0300

    fix : CORS error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,14 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
     
     'fixitapp',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
